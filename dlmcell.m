532824395f209a607f6847cff0a75151990942d2
frejon <frejon@gmail.com>
2014-01-28 10:14:17 +0100

    Clean up

--- dlmcell.m
+++ dlmcell.m
@@ -2,9 +2,10 @@ function dlmcell(file,cell_array,varargin)
 %% <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> %%
 % <><><><><>     dlmcell - Write Cell Array to Text File      <><><><><> %
 % <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> %
-%                                                 Version:    01.06.2010 %
+%                                                 Version:    2.0        %
 %                                                     (c) Roland Pfister %
-%                                             roland_pfister@t-online.de %
+%                                            updated by Fredrik Johansson&
+%                                       roland_pfister@t-online.de       %
 %                        ...with many thanks to George Papazafeiropoulos %
 %                        for his corrections and improvements.           %
 % 1. Synopsis                                                            %
@@ -27,7 +28,32 @@ function dlmcell(file,cell_array,varargin)
 %                    2, 'B', 'Banana', {'Blueberry'}; ...                %
 %                    3, 'C', 'Cherry', {'Cranberry'}; };                 %
 %         dlmcell('mytext.txt',mycell);                                  %
-%                                                                        %
+%                                                           
+% Copyright (c) 2014, Roland Pfister
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+% 
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
+%       the documentation and/or other materials provided with the distribution
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+%
 % <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> %
 
 
