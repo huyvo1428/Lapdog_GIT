a8b27b5d6bdff154226361c07de9455659a5b2f0
frejon <frejon@gmail.com>
2014-06-13 17:08:37 +0200

    807 macro filter

--- main.m
+++ main.m
@@ -80,8 +80,7 @@ opsblocks;
 if(do_mill)
 tabindexfile = sprintf('tabindex/tabindex_%s.mat',archiveid);
 fp = fopen(tabindexfile,'r');
-
-% fp = -2;
+fp = -2;
 
 if(fp > 0)
     fclose(fp);
@@ -96,7 +95,7 @@ else
   if exist('tabindex','dir')~=7
       mkdir('tabindex');
   end
-   save(tabindexfile,'tabindex');
+%   save(tabindexfile,'tabindex');
 end
 
 
