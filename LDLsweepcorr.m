0eb060133d59f88f2b8496a70096562523fb524e
frejon <frejon@gmail.com>
2014-06-23 19:04:30 +0200

    LDLsweepcorr again

--- LDLsweepcorr.m
+++ LDLsweepcorr.m
@@ -1,73 +1,95 @@
-function [curr] = LDLsweepcorr(curArray,inter,potbias,nSteps)
+function [outcurr] = LDLsweepcorr(curArray,potbias,nSteps)
+
+%function [curr] = LDLsweepcorr(curArray,inter,potbias,nSteps)
 
 
 diag = 1;
 
-curStd = accumarray(inter,curArray,[],@std);
 
 
 
-%accumarray(
-%inter(
+%curTemp = accumarray(inter,curArray,[],@mean);
 
-curTemp = accumarray(inter,curArray,[],@mean);
-
-B=find(curStd(:)>mean(curStd));
+%B=find(curStd(:)>mean(curStd));
 
 %temp2 = curTemp;
 %temp2(B)=[];
 
 
-for i=1:length(B)
+%for i=1:length(B)
 
-    A=[curArray((B(i):B(i)+nSteps-1))]; %A is all current values on suspicious LDL distrubed potatial step
+%    A=[curArray((B(i):B(i)+nSteps-1))]; %A is all current values on suspicious LDL distrubed potatial step
     
-    stdA=std(A);
+%    stdA=std(A);
   %  F = find( A(:) > 
     
     
-end
+%end
 
 
 
 
 
 
-[A,pad] = vec2mat(curArray,nSteps,NaN);
+[A,junk] = vec2mat(curArray,nSteps,NaN); %reformat curArray to matrix, fill with NaN values if needed on last steps
 
 count=A.';
-
-extOutlier = abs(count - mean(curArray)) > 3*std(curArray);
+extOutlier = abs(count - mean(curArray)) > 3*std(curArray); %extreme outliers lies outside of 99% confidence for the entire sweep
 
 count(extOutlier)= NaN; %need to exclude from mean & std calculations
 
-
-
 mu = nanmean(count); %function that ignores NaN values
 sigma = nanstd(count); %function that ignores NaN values
-[n,p] = size(count);
-% Create a matrix of mean values by
-% replicating the mu vector for n rows
+[n,junk] = size(count);
+% Create a matrix of mean & std values by replicating the mu/std vector for n rows
 MeanMat = repmat(mu,n,1);
-% Create a matrix of standard deviation values by
-% replicating the sigma vector for n rows
 SigmaMat = repmat(sigma,n,1);
 
 
-% Create a matrix of zeros and ones, where ones indicate
-% the location of outliers
+% Create a matrix of zeros and ones, where ones indicate the location of outliers
 outliers = abs(count - MeanMat) > 1*SigmaMat; % 66% chance on normal sigma mode
 
-outliers(extOutlier) = 1; % all extreme outliers should also be in this array
-
-% Calculate the number of outliers in each column
-nout = sum(sum(outliers));
-
-figure(162);
-plot(count(~outliers));
+%outliers(extOutlier) = 1; % all extreme outliers should also be in this array% no need
 
 
 count(outliers) = NaN;
+
+outcurr = nanmean(count); %final product
+
+
+
+if (diag)
+
+nout = sum(sum(outliers));
+
+figure(162);
+
+        subplot(1,2,1)
+	plot(potbias,outcurr);
+        xlabel('Vp [V]');
+        ylabel(‘I’);
+
+        title(‘edited sweep’);
+        grid on;
+        subplot(1,2,2)
+	plot(potbias,curArray) 
+        xlabel('Vp [V]');
+        ylabel(‘I’);
+        title(‘unedited sweep‘);
+        grid on;
+
+
+%plot mean(curArray) + 3*std(curArray) !!!
+
+
+
+
+
+
+curStd = accumarray(inter,curArray,[],@std);
+
+end
+
 
 
 % A=zeros(length(potbias),nSteps);
@@ -92,18 +114,8 @@ A=[curArray(i:i+nSteps-1)];
 
 
 
-figure(160);
-plot(potbias,curTemp);
-
-
-figure(161);
-plot(curStd);
-
-
 'hello'
 
 
-
-curr = nanmean(count);
 
 end
