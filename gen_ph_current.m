94a0e381838709d007c40535622e8ae9730cbf44
frejon <frejon@gmail.com>
2014-11-18 12:10:48 +0100

    gen_ph_current

---
+++ gen_ph_current.m
@@ -0,0 +1,33 @@
+function [Iph] = gen_ph_current(V,Vbplasma,Iph0,Tph,model)
+
+ind=find(V-Vbplasma>0,1,'first');
+Iph = V;
+Iph(:) = Iph0; % if V is a column/row vector, Iph is now a column/row vector of same size
+
+
+
+%in regards to models, read "ROSETTA LANGMUIR PERFORMANCE by Fredrik
+%Johansson" or Rejean Grard
+
+
+if model == 1  % photoelectrons from a sphere
+    
+
+       
+    Iph(ind:end) =Iph0*exp((V-Vbplasma)/Tph);
+   
+elseif model == 2 %photoelectrons from a point
+        
+       
+    Iph(ind:end) =Iph0*((V-Vbplasma)/Tph)*exp((V-Vbplasma)/Tph);
+    
+end
+
+
+
+end
+
+
+
+
+
