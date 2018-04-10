7cf7ab97beb655414bc1447f738624a01e339e8f
Erik P G Johansson <erik.johansson@irfu.se>
2018-04-10 12:50:55 +0200

    Bugfix: +KVPL/add_copy_of_kv_pair referenced private MATLAB code

--- +lib_shared_EJ/+KVPL/add_copy_of_kv_pair.m
+++ +lib_shared_EJ/+KVPL/add_copy_of_kv_pair.m
@@ -7,7 +7,7 @@ function   kvlDest = add_copy_of_kv_pair(kvlSrc, kvlDest, key)
 % PROPOSAL: New name. 
 % PROPOSAL: Rationalize away this function?
 
-    value   = EJ_utils.KVPL.read_value( kvlSrc,  key);
-    kvlDest = EJ_utils.KVPL.add_kv_pair(kvlDest, key, value);
+    value   = lib_shared_EJ.KVPL.read_value( kvlSrc,  key);
+    kvlDest = lib_shared_EJ.KVPL.add_kv_pair(kvlDest, key, value);
     
 end
