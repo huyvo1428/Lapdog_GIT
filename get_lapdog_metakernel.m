21ebcf5eb3eafececb6aaf92285ba69b4c5cbc40
Erik P G Johansson <erik.johansson@irfu.se>
2019-05-13 11:21:20 +0200

    Bugfix: get_lapdog_metakernel.m: error handling

--- get_lapdog_metakernel.m
+++ get_lapdog_metakernel.m
@@ -1,19 +1,23 @@
-% Return Lapdog default metakernel. Use default file in Lapdog directory which the rest of Lapdog uses.
+%
+% Return the metakernel that _Lapdog_ uses/would use. Always returns the default file in the Lapdog directory which
+% the rest of Lapdog uses.
 % 
 % ASSUMPTION: Current script is located in Lapdog directory.
-% NOTE: Partly created so that code external to Lapdog can easily obtain the Lapdog metakernel.
+% NOTE: Partly created so that code external to Lapdog can easily obtain the Lapdog metakernel (e.g. if it uses another
+% current directory).
 %
 %
-% Initially created by Erik P G Johansson, IRF Uppsala.
+% Initially created 2018-11-01 by Erik P G Johansson, IRF Uppsala.
+%
 function metakernel = get_lapdog_metakernel()
     % PROPOSAL: Use readlink -f to de-reference symlinks?!!
+    % PROPOSAL: Additionally return Lapdog directory.
     
     currentMFile = mfilename('fullpath');
     [lapdogDir, basenameJunk, extJunk] = fileparts(currentMFile);
     metakernel = fullfile(lapdogDir, 'metakernel_rosetta.txt');
     if ~exist(metakernel, 'file')
-        error('Can not find kernel file "%s"', metakernelFile)
-        % Call error too?
+        error('Can not find metakernel file "%s"', metakernel)
     end
     
 end
